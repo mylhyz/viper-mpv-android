8db41904dffb4e69241ffd2dbccb51789d252643
mylhyz <mylhyz@gmail.com>
2024-03-15 18:01:20 +0800

    添加PlayerView占位

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(project(":mpv-view"))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ mpv-view/src/main/java/io/viper/android/mpv/view/PlayerView.kt
@@ -0,0 +1,9 @@
+package io.viper.android.mpv.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class PlayerView(content: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    FrameLayout(content, attrs, defStyleAttr) {
+}

6e28c951255b0b3d5a337f89ed2c2e010745628a
mylhyz <mylhyz@gmail.com>
2024-03-21 09:04:45 +0800

    完善setting界面

--- app/src/main/java/io/viper/android/mpv/player/SettingsFragment.kt
+++ app/src/main/java/io/viper/android/mpv/player/SettingsFragment.kt
@@ -3,8 +3,45 @@ package io.viper.android.mpv.player
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment: PreferenceFragmentCompat(){
+class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+
+    class GeneralPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_general, rootKey)
+        }
+    }
+
+    class VideoPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_video, rootKey)
+        }
+    }
+
+    class UIPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_ui, rootKey)
+        }
+    }
+
+    class GesturesPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_gestures, rootKey)
+        }
+    }
+
+    class DeveloperPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_developer, rootKey)
+        }
+    }
+
+    class AdvancedPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_advanced, rootKey)
+        }
     }
 }

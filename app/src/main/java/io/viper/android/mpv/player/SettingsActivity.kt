7ccbd3a986c037de8d94b14f334bce2ba1174256
mylhyz <mylhyz@gmail.com>
2024-03-21 09:40:44 +0800

    优化设置界面导航逻辑

--- app/src/main/java/io/viper/android/mpv/player/SettingsActivity.kt
+++ app/src/main/java/io/viper/android/mpv/player/SettingsActivity.kt
@@ -2,8 +2,11 @@ package io.viper.android.mpv.player
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsActivity : AppCompatActivity(R.layout.activity_frag_container) {
+class SettingsActivity : AppCompatActivity(R.layout.activity_frag_container),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +15,29 @@ class SettingsActivity : AppCompatActivity(R.layout.activity_frag_container) {
             add(R.id.fragment_container_view, SettingsFragment())
             commit()
         }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                // 当页面内部返回时，恢复settings的标题
+                setTitle(R.string.title_activity_settings)
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (supportFragmentManager.popBackStackImmediate()) {
+            // 防止在子fragment中点击返回键直接返回指定的父activity
+            return true
+        }
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        // 拦截子fragment打开，并设置当前title
+        setTitle(pref.title)
+        return false
     }
 }
